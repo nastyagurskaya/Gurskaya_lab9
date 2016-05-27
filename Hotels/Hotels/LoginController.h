9300d9352f0df3b0ebc0cedd23d79b41630ae16c
Nastya <just.gurskaya@gmail.com>
2016-05-27 22:32:06 +0300

    no message

---
+++ Hotels/Hotels/LoginController.h
@@ -0,0 +1,13 @@
+//
+//  LoginController.h
+//  Hotels
+//
+//  Created by Admin on 27.05.16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginController : UIViewController
+
+@end
